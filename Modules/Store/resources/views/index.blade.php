31848a92cde26a666c5f8af4f1d756e611a58534
Hachimankira <kiran9860819025@gmail.com>
2024-05-02 17:30:55 +0545

    js library problem solved and some changes in home page

--- Modules/Store/resources/views/index.blade.php
+++ Modules/Store/resources/views/index.blade.php
@@ -1,5 +1,5 @@
 @php
-use Illuminate\Support\Str;
+    use Illuminate\Support\Str;
 @endphp
 <x-app-layout>
     <x-slot name="header">
@@ -198,26 +198,7 @@ use Illuminate\Support\Str;
                         <div class="col-lg-9">
                             <div class="row g-4 justify-content-center">
                                 @foreach ($products as $product)
-                                    <div class="col-md-6 col-lg-6 col-xl-4">
-                                        <div class="rounded position-relative fruite-item">
-                                            <div class="fruite-img">
-                                                <img src="img/bike.jpg" class="img-fluid w-100 rounded-top"
-                                                    alt="">
-                                            </div>
-                                            <div class="text-white bg-secondary px-3 py-1 rounded position-absolute"
-                                                style="top: 10px; left: 10px;">{{ $product->type }}</div>
-                                            <div class="p-4 border border-secondary border-top-0 rounded-bottom">
-                                                <h5>{{ Str::limit("$product->year $product->brand $product->model", 18) }}</h5>
-                                                <p class="text-dark fs-8 mb-0">{{ $product->distance }}km &bull; {{ $product->body_type }} &bull; {{ $product->owner }}</p>                                                <div class="d-flex justify-content-between flex-lg-wrap">
-                                                    <p class="text-dark fs-5 fw-bold mb-0">{{ $product->price }}</p>
-                                                    <a href="#"
-                                                        class="btn border border-secondary rounded-pill px-3 text-primary"><i
-                                                            class="fa fa-shopping-bag me-2 text-primary"></i> Add to
-                                                        cart</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <x-card-shop :product="$product" />
                                 @endforeach
 
                                 <div class="col-12">
